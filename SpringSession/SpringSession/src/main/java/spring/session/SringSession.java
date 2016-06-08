6113df194105beee60f2c0d7f00190a4af34e5bb
sotheareth <ham8086sotheareth@yahoo.com>
2016-06-08 15:13:26 +0700

    create new spring session data with redis

---
+++ SpringSession/SpringSession/src/main/java/spring/session/SringSession.java
@@ -0,0 +1,41 @@
+package spring.session;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/main")
+public class SringSession {
+
+	private static final String NAME = "name";
+
+	@RequestMapping(value = "/hi", method = RequestMethod.GET)
+	@ResponseBody
+	public String hi(HttpServletRequest req, HttpServletResponse resp) {
+		HttpSession session = req.getSession();
+		String name = "Word";
+		if(session.getAttribute(NAME) !=  null){
+			name = (String) session.getAttribute(NAME);
+		}
+
+		// create a greeting with the name
+		String greeting = String.format("Hello %s!", name);
+
+		return greeting;
+	}
+
+	@RequestMapping(value = "/hi", method = RequestMethod.POST)
+	@ResponseBody
+	public String postHi(HttpServletRequest req, HttpServletResponse resp) {
+		String name = req.getParameter(NAME);
+		// store the name variable as a session attribute
+		req.getSession().setAttribute(NAME, name);
+		return name;
+	}
+}
